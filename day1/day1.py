b242ad7e1b58a9e301c97d02a76782928671295a
Augustin Bossart <augustin.bossart@up42.com>
2024-12-03 13:32:41 +0100

    compute similarity

--- day1/day1.py
+++ day1/day1.py
@@ -31,16 +31,31 @@ def compute_distance(list1, list2):
             distance_list.append(b-a)
     print(f"the full distance is: {sum(distance_list)}")
 
+
+def compute_similarity(list1, list2):
+    similarity_list =[]
+    for a in list1:
+        count = 0
+        for b in list2:
+            if a == b:
+                count += 1
+        similarity_list.append(a*count)
+    print(similarity_list)
+    print(f"the similarity score is: {sum(similarity_list)}")
+
+
 def main():
     """
     Main function to execute the program logic.
     """
-    col1, col2 = load_data("day1.txt")
+    col1, col2 = load_data("day1/day1.txt")
 
     sorted_list1 = sort_list(col1)
     sorted_list2= sort_list(col2)
     compute_distance(sorted_list1, sorted_list2)
-    print("completed.")
+    print("completed step 1")
+
+    compute_similarity(sorted_list1, sorted_list2)
 
 
 if __name__ == "__main__":
